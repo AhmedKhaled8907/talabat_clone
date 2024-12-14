e074faf7acfe73a0f87dce1ea7c0ae3e5bcfbeb3
Ahmed Khaled <ahmedkh8907@gmail.com>
2024-12-14 17:29:45 +0200

    adding fonts & generate assets

---
+++ lib/core/utils/resources/app_assets.dart
@@ -0,0 +1,139 @@
+// ignore_for_file: prefer_single_quotes
+class AppAssets {
+  AppAssets._();
+  
+  static const String fontsDMSansBold = "assets/fonts/DMSans-Bold.ttf";
+
+  static const String fontsDMSansMedium = "assets/fonts/DMSans-Medium.ttf";
+
+  static const String fontsDMSansRegular = "assets/fonts/DMSans-Regular.ttf";
+
+  static const String fontsDMSansSemiBold = "assets/fonts/DMSans-SemiBold.ttf";
+
+  static const String imagesAuthApple = "assets/images/auth/apple.svg";
+
+  static const String imagesAuthBaselineEmail = "assets/images/auth/baseline_email.svg";
+
+  static const String imagesAuthFacebook = "assets/images/auth/facebook.svg";
+
+  static const String imagesAuthGoogle = "assets/images/auth/google.svg";
+
+  static const String imagesAuthStartPage = "assets/images/auth/start_page.svg";
+
+  static const String imagesFlagsBahrain = "assets/images/flags/bahrain.svg";
+
+  static const String imagesFlagsEgypt = "assets/images/flags/egypt.svg";
+
+  static const String imagesFlagsIraq = "assets/images/flags/iraq.svg";
+
+  static const String imagesFlagsJordan = "assets/images/flags/jordan.svg";
+
+  static const String imagesFlagsKuwait = "assets/images/flags/kuwait.svg";
+
+  static const String imagesFlagsOman = "assets/images/flags/oman.svg";
+
+  static const String imagesFlagsQatar = "assets/images/flags/qatar.svg";
+
+  static const String imagesFlagsUae = "assets/images/flags/uae.svg";
+
+  static const String imagesFoodBestSellers = "assets/images/food/best_sellers.svg";
+
+  static const String imagesFoodCuisines = "assets/images/food/cuisines.svg";
+
+  static const String imagesFoodExceptional = "assets/images/food/Exceptional.svg";
+
+  static const String imagesFoodHealthy = "assets/images/food/healthy.svg";
+
+  static const String imagesFoodJunior = "assets/images/food/junior.svg";
+
+  static const String imagesFoodMalak = "assets/images/food/malak.svg";
+
+  static const String imagesFoodMustTries = "assets/images/food/must_tries.svg";
+
+  static const String imagesFoodOfferup = "assets/images/food/offerup.svg";
+
+  static const String imagesFoodPapparoti = "assets/images/food/papparoti.svg";
+
+  static const String imagesFoodPizza2Go = "assets/images/food/pizza_2_go.svg";
+
+  static const String imagesFoodPokeAndGo = "assets/images/food/poke_and_go.svg";
+
+  static const String imagesFoodStar = "assets/images/food/star.svg";
+
+  static const String imagesFoodSubway = "assets/images/food/subway.svg";
+
+  static const String imagesFoodSuperSaver = "assets/images/food/super_saver.svg";
+
+  static const String imagesFoodSushiCounter = "assets/images/food/sushi_counter.svg";
+
+  static const String imagesFoodTalabatPro = "assets/images/food/talabat_pro.svg";
+
+  static const String imagesHomeAlloBeirut = "assets/images/home/allo_beirut.svg";
+
+  static const String imagesHomeBarbar = "assets/images/home/barbar.svg";
+
+  static const String imagesHomeBestSellers = "assets/images/home/best_sellers.svg";
+
+  static const String imagesHomeCasseroleImage = "assets/images/home/casserole_image.svg";
+
+  static const String imagesHomeCode = "assets/images/home/code.svg";
+
+  static const String imagesHomeCoffee = "assets/images/home/coffee.svg";
+
+  static const String imagesHomeFalafil = "assets/images/home/falafil.svg";
+
+  static const String imagesHomeFlower = "assets/images/home/flower.svg";
+
+  static const String imagesHomeFood = "assets/images/home/food.svg";
+
+  static const String imagesHomeGiveBack = "assets/images/home/give_back.svg";
+
+  static const String imagesHomeGroceries = "assets/images/home/groceries.svg";
+
+  static const String imagesHomeHealth = "assets/images/home/health.svg";
+
+  static const String imagesHomeLaffah = "assets/images/home/laffah.svg";
+
+  static const String imagesHomeMoreShops = "assets/images/home/more_shops.svg";
+
+  static const String imagesHomeMustTries = "assets/images/home/must_tries.svg";
+
+  static const String imagesHomePastOrders = "assets/images/home/past_orders.svg";
+
+  static const String imagesHomePhone = "assets/images/home/phone.svg";
+
+  static const String imagesHomeSortBy = "assets/images/home/sort_by.svg";
+
+  static const String imagesHomeSuperSaver = "assets/images/home/super_saver.svg";
+
+  static const String imagesHomeTalabatMart = "assets/images/home/talabat_mart.svg";
+
+  static const String imagesSettingsAboutApp = "assets/images/settings/about_app.svg";
+
+  static const String imagesSettingsGetHelp = "assets/images/settings/get_help.svg";
+
+  static const String imagesSettingsNotifications = "assets/images/settings/notifications.svg";
+
+  static const String imagesSettingsOffers = "assets/images/settings/offers.svg";
+
+  static const String imagesSettingsTalabatPay = "assets/images/settings/talabat_pay.svg";
+
+  static const String imagesSettingsYourOrders = "assets/images/settings/your_orders.svg";
+
+  static const String imagesSplashLogo = "assets/images/splash/logo.svg";
+
+  static const String imagesSplashLogoOrange = "assets/images/splash/logo_orange.svg";
+
+  static const String imagesSplashLogoT = "assets/images/splash/logo_t.svg";
+
+  static const String imagesTalabatMartArabicTaste = "assets/images/talabat_mart/arabic_taste.svg";
+
+  static const String imagesTalabatMartEverydayDiscounts = "assets/images/talabat_mart/everyday_discounts.svg";
+
+  static const String imagesTalabatMartImported = "assets/images/talabat_mart/imported.svg";
+
+  static const String imagesTalabatMartTalabatMart = "assets/images/talabat_mart/talabat_mart.svg";
+
+  static const String imagesTalabatMartTodayBest = "assets/images/talabat_mart/today_best.svg";
+}
+
